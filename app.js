03cf509038db6b46707bef8355853458dd0be761
unknown <daveasmah@yahoo.com>
2022-07-13 22:46:49 -0400

    Second commit

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 //Define a function that takes in an array of numbers/integers 
 //and returns the sorted version of that array, greatest to least. 
 //(Note: do NOT use the "reverse" method.)
+
+
+const array1 = [-1, 1, 30, 4, 21, 100000, -5];
+
+//Write a function that accepts an array as a parameter
+function sortedArray(num){
+
+//Write a function that with 2 elements of comparison (a, b)    
+    return num.sort(function(a, b){
+//sort the array in descending order        
+        return b - a;
+      });
+      
+}
+
+console.log(sortedArray(array1));
